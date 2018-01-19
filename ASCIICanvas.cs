dfe88c011af12d4cdea3d56f03eeb162e4545f67
torvnen <torvnen@gmail.com>
2018-01-19 14:20:31 +0200

    Initial commit

---
+++ ASCIICanvas.cs
@@ -0,0 +1,261 @@
+﻿namespace Demo
+{
+    using System;
+    using System.Drawing;
+
+    /// <summary>
+    /// This shall be the viewable part
+    /// </summary>
+    class ASCIICanvas : IDisposable
+    {
+
+        #region Settings class
+
+        public class ASCIICanvasSettings
+        {
+
+            /// <summary>
+            /// Basic instance creation. 
+            /// </summary>
+            /// <param name="background"><see cref="Background"/></param>
+            /// <param name="foreground"><see cref="Foreground"/></param>
+            /// <param name="border"><see cref="Border"/></param>
+            public ASCIICanvasSettings(
+                char background = '.',
+                char foreground = 'X',
+                char border = 'O',
+                short width = 28,
+                short height = 10)
+            {
+                Background = background;
+                Foreground = foreground;
+                Border = border;
+                Width = width;
+                Height = height;
+            }
+
+            /// <summary>
+            /// Symbol to be used as the texture of the background
+            /// </summary>
+            public readonly char Background;
+
+            /// <summary>
+            /// Symbol to be used as the texture of the foreground,
+            /// meaning the 'cursor' character
+            /// </summary>
+            public readonly char Foreground;
+
+            /// <summary>
+            /// Symbol to be used as the border of the canvas
+            /// </summary>
+            public readonly char Border;
+
+            /// <summary>
+            /// Height of the canvas
+            /// </summary>
+            public readonly short Height;
+
+            /// <summary>
+            /// Width of the canvas
+            /// </summary>
+            public readonly short Width;
+
+        }
+
+        #endregion Settings class
+
+        /// <summary>
+        /// Which directions the characters can move towards
+        /// </summary>
+        [Flags]
+        public enum Directions
+        {
+            Up,
+            Down,
+            Left,
+            Right
+        }
+
+        /// <summary>
+        /// Parameters for drawing the picture.
+        /// </summary>
+        public readonly ASCIICanvasSettings Settings;
+
+        /// <summary>
+        /// Character (cursor) location
+        /// </summary>
+        private int _cursorPositionX = 1;
+
+        /// <summary>
+        /// Character (cursor) location
+        /// </summary>
+        private int _cursorPositionY = 1;
+
+        /// <summary>
+        /// Where the 'monster' or 'apple' is located
+        /// </summary>
+        private Point _monsterLocation;
+
+        /// <summary>
+        /// <see cref="_cursorPositionX"/>
+        /// TODO Use Point struct.
+        /// </summary>
+        public int CursorPositionX
+        {
+            get { return _cursorPositionX; }
+            set
+            {
+                _cursorPositionX =
+                    // Value inside allowed bounds? 
+                    (value > Settings.Width - 2) ? (Settings.Width - 2) :
+                    (value < 1) ? 1 : value;
+            }
+        }
+
+        /// <summary>
+        /// <see cref="_cursorPositionY"></see>
+        /// TODO Use Point struct.
+        /// </summary>
+        public int CursorPositionY
+        {
+            get { return _cursorPositionY; }
+            set
+            {
+                _cursorPositionY =
+                    // Value inside allowed bounds? 
+                    (value > Settings.Height - 2) ? (Settings.Height - 2) :
+                    (value < 1) ? 1 : value;
+            }
+        }
+        
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        /// <param name="settings">[Optional, will use default values if null or empty]</param>
+        public ASCIICanvas(ASCIICanvasSettings settings = null)
+        {
+            Settings = settings ?? new ASCIICanvasSettings();
+        }
+
+        /// <summary>
+        /// Use this to draw the initial form
+        /// </summary>
+        public void Initiate()
+        {
+            Console.Clear();
+            Console.CursorLeft = 0;
+            Console.CursorTop = 0;
+            for (var y = 0; y < Settings.Height; y++)
+            {
+                for (var x = 0; x < Settings.Width; x++)
+                {
+
+                    // All right! Let's do the drawing!
+                    Console.Write
+                    (
+                        (
+                            // Is this the first or the last column?
+                            x == 0 || x == (Settings.Width - 1)
+                            ||
+                            // Is this the first or the last row?
+                            y == 0 || y == (Settings.Height - 1)
+                        ) ? Settings.Border : Settings.Background
+                    );
+                }
+                Console.Write(Environment.NewLine);
+            }
+
+            // Spawn something to 'catch' to the game
+            _monsterLocation = SpawnMonster();
+
+            Console.CursorLeft = CursorPositionX;
+            Console.CursorTop = CursorPositionY;
+        }
+
+        /// <summary>
+        /// Adds something on the playing field for the player
+        /// </summary>
+        /// <returns>Location of new monster</returns>
+        private Point SpawnMonster()
+        {
+            var rng = new Random();
+            var randomX = rng.Next(1, Settings.Width - 2);
+            var randomY = rng.Next(1, Settings.Height - 2);
+
+            if (randomX != CursorPositionX || randomY != CursorPositionY)
+            {
+                Console.CursorLeft = randomX;
+                Console.CursorTop = randomY;
+                Console.Write('*');
+
+                return new Point(randomX, randomY);
+            }
+            else
+            {
+                // Try again, bad luck
+                return SpawnMonster();
+            }
+        }
+
+        /// <summary>
+        /// IDisposable implementation.
+        /// </summary>
+        public void Dispose()
+        {
+            Console.Clear();
+        }
+
+        /// <summary>
+        /// Perform a single-character write and move cursor back one unit
+        /// </summary>
+        /// <param name="c">Char to write</param>
+        private void Write(char c)
+        {
+            // Take previous or initial cursor position:
+            Console.CursorLeft = _cursorPositionX;
+            Console.CursorTop = _cursorPositionY;
+
+            // Perform writing, move cursor back to compensate for the movement.
+            Console.Write(c);
+            Console.CursorLeft -= 1;
+        }
+
+        /// <summary>
+        /// Moves the cursor
+        /// </summary>
+        /// <param name="direction"></param>
+        internal void Move(Directions direction)
+        {
+            // Overwrite current cursor
+            Write(Settings.Background);
+
+            // Perform the actual requested movement
+            switch (direction)
+            {
+                case Directions.Up:
+                    CursorPositionY -= 1;
+                    break;
+                case Directions.Down:
+                    CursorPositionY += 1;
+                    break;
+                case Directions.Left:
+                    CursorPositionX -= 1;
+                    break;
+                case Directions.Right:
+                    CursorPositionX += 1;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+            
+            // Did we catch the apple?
+            if (CursorPositionX == _monsterLocation.X && CursorPositionY == _monsterLocation.Y)
+            {
+                _monsterLocation = SpawnMonster();
+            }
+
+            // Write character
+            Write(Settings.Foreground);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,133 @@
+﻿namespace Demo
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// This program creates a matrix of ASCII characters
+    /// which are navigatable through keyboard input by another
+    /// ASCII character which acts as the cursor.
+    /// 
+    /// - NO LINQ. 
+    /// - ONLY.NET Core 2.0 compatible packages!
+    /// - ONLY new code. No copy-paste!
+    /// 
+    /// @author Niko Torvinen
+    /// 
+    /// </summary>
+    class Program
+    {
+
+        /// <summary>
+        /// Which key was pressed.
+        /// </summary>
+        private static ConsoleKeyInfo _keyinfo;
+        
+        /// <summary>
+        /// The matrix of ASCII characters; 
+        /// this is what will be drawn on the terminal console.
+        /// </summary>
+        private static ASCIICanvas _view = new ASCIICanvas();
+
+        private static string _utilityName = "Simple snake game";
+
+        /// <summary>
+        /// Possible command line arguments and mappings to their actions
+        /// </summary>
+        private static Dictionary<string, Action> _switches = new Dictionary<string, Action>
+        {
+            {
+                "-h", () => 
+                { 
+                    // Print instructions:
+                    Console.Write(_helptext);
+                }
+            },
+            {
+                "-p", () =>
+                {
+                    // Play the mini-game
+                    PlayGame();
+                }
+            }
+        };
+
+        /// <summary>
+        /// The instructions for the program.
+        /// </summary>
+        private static string _helptext {
+            get
+            {
+                var keys = new string[_switches.Keys.Count];
+                _switches.Keys.CopyTo(keys, 0);
+                var sb = new StringBuilder();
+                foreach (string key in keys)
+                {
+                    sb.Append($"[{key}]");
+                }
+
+                return 
+                    $"{_utilityName}{sb}{Environment.NewLine}" +
+                    "Press ESCAPE to quit.";
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length == 1)
+            {
+                args = new[] { "-p" };
+            }
+
+            foreach (var arg in args)
+            {
+                if (_switches.ContainsKey(arg))
+                {
+                    _switches[arg]?.Invoke();
+                }
+            }
+
+            Console.WriteLine("Goodbye! :)");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Draw a canvas and start playing!
+        /// </summary>
+        static void PlayGame()
+        {
+
+            using (var canvas = new ASCIICanvas())
+            {
+                var keybindings = new Dictionary<ConsoleKey, Action>
+                {
+                    { ConsoleKey.UpArrow, () => { canvas.Move(ASCIICanvas.Directions.Up); } },
+                    { ConsoleKey.DownArrow, () => { canvas.Move(ASCIICanvas.Directions.Down); } },
+                    { ConsoleKey.LeftArrow, () => { canvas.Move(ASCIICanvas.Directions.Left); } },
+                    { ConsoleKey.RightArrow, () => { canvas.Move(ASCIICanvas.Directions.Right); } }
+                };
+
+                // Draw initial canvas!
+                canvas.Initiate();
+                
+                do
+                {
+                    _keyinfo = Console.ReadKey();
+
+                    if (keybindings.ContainsKey(_keyinfo.Key))
+                    {
+                        keybindings[_keyinfo.Key].Invoke();
+                    }
+                    else
+                    {
+
+                        // TODO handle invalid input
+                    }
+
+                }
+                while (_keyinfo.Key != ConsoleKey.Escape);
+            }
+        }
+    }
+}
